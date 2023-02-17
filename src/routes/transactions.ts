94cb1e169ad1a4216a0086060ad80ed4ec713b76
lucianobs1 <lucianotetsu@gmail.com>
2023-02-17 07:59:44 -0300

    transactions: create feature to add a new transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,19 +1,28 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import crypto from 'node:crypto'
 
 import { knex } from '../database'
 
 async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'Transação de teste',
-        amount: 1000,
-      })
-      .returning('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transaction
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
 
