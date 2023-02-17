ab65fc4d350ce03efcc74c12fdefa5449e6f3db4
lucianobs1 <lucianotetsu@gmail.com>
2023-02-17 06:15:52 -0300

    database - create environment variable

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,13 @@
 import { knex as setupKnex, Knex } from 'knex'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL env not found')
+}
+
 const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import crypto from 'node:crypto'
 import fastify from 'fastify'
 import { knex } from './database'
